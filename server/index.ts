3233f12aa2e5f6714a5493fbf5ff3e4307547a9d
SOMASEKAR17 <somasekarnaidu79@gmail.com>
2025-08-13 23:38:59 +0530

    first commit

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -60,12 +63,19 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  const port = Number(process.env.PORT) || 5000;
+
+  const listenOptions: any = {
+  port,
+  host: "0.0.0.0",
+};
+
+if (process.platform !== "win32") {
+  listenOptions.reusePort = true;
+}
+
+server.listen(listenOptions, () => {
+  log(`serving on port ${port}`);
+});
+
 })();
